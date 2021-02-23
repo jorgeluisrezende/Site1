acd8ad3b17b602ef9581b28add6742f607b462d4
wally-jnyor <arealunar9@gmail.com>
2021-02-23 11:00:15 -0300

    :package: funcionalidade de orçamento e valores

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
-var hamburguer = document.querySelector(".hamburguer");
+let hamburguer = document.querySelector(".hamburguer");
 
 hamburguer.addEventListener("click", function(){
     document.querySelector(".container").classList.toggle("show-menu");
 })
 
+
+document.querySelector("#quantidade").addEventListener("change", attValue)
+document.querySelector("#js").addEventListener("change", attValue)
+document.querySelector("#layout-sim").addEventListener("change", attValue)
+
+function attValue() {
+    const quantidade = document.querySelector("#quantidade").value
+    const needJS = document.querySelector("#js").checked
+    const needLayout = document.querySelector("#layout-sim").checked
+    let valorTotal = quantidade * 100;
+    
+    if(needJS) {valorTotal *= 1.1}
+    if(needLayout) {valorTotal += 500}
+
+    document.querySelector("#valorTotal").innerHTML = `R$ ${valorTotal.toFixed(2)}`
+    console.log(quantidade)
+}
