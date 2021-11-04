f28298ebcf5083b2ef488fe7dc6b4c7e8472ca4f
wally-jnyor <arealunar9@gmail.com>
2021-11-04 00:37:08 -0300

    :package: atualização sidebar.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // ========== Menu "hamburguer" ===========
 let hamburguer = document.querySelector(".hamburguer");
+let fecharSideBar = document.querySelector(".fechaMenu");
 // ========== Scroll top ===========
 let link_top = document.getElementById('link-topo');
 
@@ -8,6 +9,11 @@ let link_top = document.getElementById('link-topo');
 // ========== Menu "hamburguer" ===========
 hamburguer.addEventListener("click", function () {
     document.querySelector(".container").classList.toggle("show-menu");
+    document.querySelector('.fechaMenu').classList.toggle('isClose');
+})
+fecharSideBar.addEventListener("click", function () {
+    document.querySelector(".container").classList.toggle("show-menu");
+    document.querySelector(".fechaMenu").classList.toggle('isClose');
 })
 
 // ========== Scroll top ===========
