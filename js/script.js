02996f363d33352608291a3de3f6f0d2da43fc1e
wally-jnyor <arealunar9@gmail.com>
2021-11-09 09:21:01 -0300

    :package: atualizzação

--- js/script.js
+++ js/script.js
@@ -2,12 +2,6 @@
 let hamburguer = document.querySelector(".hamburguer");
 let fecharSideBar = document.querySelector(".fechaMenu");
 
-// ========== Scroll top ===========
-let link_top = document.getElementById('link-topo');
-
-
-
-// ========== Menu "hamburguer" ===========
 hamburguer.addEventListener("click", function () {
     document.querySelector(".container").classList.toggle("show-menu");
     document.querySelector('.fechaMenu').classList.toggle('isClose');
@@ -17,7 +11,27 @@ fecharSideBar.addEventListener("click", function () {
     document.querySelector(".fechaMenu").classList.toggle('isClose');
 })
 
+
+// ========== botão para exibir div de projetos ===========
+let div_projetos_hidden = document.querySelector('.projetos_hidden');
+let Hidden_cards = document.querySelectorAll('.hidden_cards');
+let btn_open_div = document.querySelector('.btn_open');
+
+btn_open_div.addEventListener('click', () => {
+    if(div_projetos_hidden.style.display == 'none') {
+        div_projetos_hidden.style.display = 'flex';
+    } else {
+        div_projetos_hidden.style.display = 'none';
+        Hidden_cards.forEach(card => {
+            
+        })
+    }
+})
+
+
 // ========== Scroll top ===========
+let link_top = document.getElementById('link-topo');
+
 window.onscroll = () => {
     if (document.body.scrollTop !=0 || document.documentElement.scrollTop != 0) {
         link_top.style.opacity = '.8';
